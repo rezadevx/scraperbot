a771f50eef34909903119dfe1d4442003aa18ac3
rezadevs <sawe57192@gmail.com>
2025-07-01 19:16:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = 23855532         # Ganti dengan API ID kamu dari https://my.telegram.org
-API_HASH = "3cc6eac0a9fbfe0b2b1da77f043cc9c9"
-BOT_TOKEN = "7845877139:AAElEEVKwp55dZ8yg1vvn-Ljb_JwtnGqjPg"
+API_ID = apiidkamu        # Ganti dengan API ID kamu dari https://my.telegram.org
+API_HASH = "monyetkamu"
+BOT_TOKEN = "bottokenkamu"
